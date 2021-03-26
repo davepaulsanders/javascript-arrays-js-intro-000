0947bc4b6064928edad9ed27872cd072c4afdb51
David P Sanders <davepaulsanders@gmail.com>
2021-03-26 18:36:33 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,6 +11,6 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 }
 
 addElementToEndOfArray(array, element){
-  var array = [...array, element]
+  var array = [...array, element];
   return array;
 }
