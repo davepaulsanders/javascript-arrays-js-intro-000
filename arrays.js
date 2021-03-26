a49915c7ba4f1776e37c1a985d4e8d52bbd2b748
David P Sanders <davepaulsanders@gmail.com>
2021-03-26 18:54:58 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -33,6 +33,6 @@ function removeElementFromBeginningOfArray(array){
   return array.slice(1)
 }
 
-function destructivelyRemoveElementFromEndOfArray(arrray){
+function destructivelyRemoveElementFromEndOfArray(array){
   return array.pop()
 }
