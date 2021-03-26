d8cccb3664392968046115901c6091080acc715f
David P Sanders <davepaulsanders@gmail.com>
2021-03-26 18:12:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,10 +2,10 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 
 function addElementToBeginningOfArray(array, element){
-var array.push(element);
-return `${array}`
+var = ${array}.push$({element});
+return 
 }
 
 function removeElementFromEndOfArray(array){
-  return 
+  return
 }
